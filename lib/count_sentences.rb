5f25eef4da6fa8e903e528728c008f5ddc283cc7
Christine Bumpous <bumpouce@gmail.com>
2020-03-11 16:51:41 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    test = self.split(/?\. | ?\? | ?\!/)
+    test = self.split(/?\. ?\? ?\!/)
     test.each {|sentence| puts "#{self}: #{sentence}"}
   end
 end
